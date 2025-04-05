46cf675929d5c66ad3c081b7d9335d1deea11947
vaibhavphalak03 <vaibhavphalak03@gmail.com>
2025-04-05 15:52:40 +0530

    Update Majority_element.py

--- Arrays/Majority_element.py
+++ Arrays/Majority_element.py
@@ -1,3 +1,23 @@
+'''
+Majority Element II
+Difficulty: MediumAccuracy: 48.1%Submissions: 137K+Points: 4Average Time: 15m
+You are given an array of integer arr[] where each number represents a vote to a candidate. Return the candidates that have votes greater than one-third of the total votes, If there's not a majority vote, return an empty array. 
+
+Note: The answer should be returned in an increasing format.
+
+Examples:
+
+Input: arr[] = [2, 1, 5, 5, 5, 5, 6, 6, 6, 6, 6]
+Output: [5, 6]
+Explanation: 5 and 6 occur more n/3 times.
+Input: arr[] = [1, 2, 3, 4, 5]
+Output: []
+Explanation: o candidate occur more than n/3 times.
+Constraint:
+1 <= arr.size() <= 106
+-109 <= arr[i] <= 109
+'''
+
 class Solution:
     
     # Function to find the majority elements in the array
