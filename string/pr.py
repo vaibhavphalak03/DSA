ff4bf960e50b3d89a92f72685efe9d14ddbfb6a8
Abhay <113433885+abhayxxmm1@users.noreply.github.com>
2025-04-15 00:52:00 +0530

    Reverse a list of characters

--- string/pr.py
+++ string/pr.py
@@ -2,3 +2,11 @@
 print("Hello world")
 
 print("Hello world") # 2nd time!
+
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
