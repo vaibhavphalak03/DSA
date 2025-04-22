2173279b10a6d88a94847a4092764cde8d993eb8
Vaibhav Phalak <vaibhavphalak03@gmail.com>
2025-04-22 21:58:53 +0530

    Create Strings Rotations of Each Other.py

---
+++ string/Strings Rotations of Each Other.py
@@ -0,0 +1,27 @@
+'''
+You are given two strings of equal lengths, s1 and s2. The task is to check if s2 is a rotated version of the string s1.
+
+Note: The characters in the strings are in lowercase.
+
+Examples :
+
+Input: s1 = "abcd", s2 = "cdab"
+Output: true
+Explanation: After 2 right rotations, s1 will become equal to s2.
+Input: s1 = "aab", s2 = "aba"
+Output: true
+Explanation: After 1 left rotation, s1 will become equal to s2.
+Input: s1 = "abcd", s2 = "acbd"
+Output: false
+Explanation: Strings are not rotations of each other.
+Constraints:
+1 <= s1.size(), s2.size() <= 105
+'''
+
+class Solution:
+    
+    #Function to check if two strings are rotations of each other or not.
+    def areRotations(self,s1,s2):
+        s1 = s1 + s1
+        
+        return s2 in s1
