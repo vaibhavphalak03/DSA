02125a77ba1b0d274de534a4bc485436998cac9a
Vaibhav Phalak <vaibhavphalak03@gmail.com>
2025-04-24 00:02:19 +0530

    Update Find H-Index.py

--- Sortimg/Find H-Index.py
+++ Sortimg/Find H-Index.py
@@ -17,3 +17,25 @@ Constraints:
 1 ≤ citations.size() ≤ 106
 0 ≤ citations[i] ≤ 106
 '''
+class Solution:
+    # Function to find hIndex
+    def hIndex(self, citations):
+        n = len(citations)
+        freq = [0] * (n+1)
+        
+        
+        for citation in citations:
+            if citation >= n:
+                freq[n] += 1
+            else:
+                freq[citation] += 1
+                
+        idx = n 
+        
+        s = freq[n]
+        
+        while s < idx:
+            idx -= 1
+            s += freq[idx]
+            
+        return idx
